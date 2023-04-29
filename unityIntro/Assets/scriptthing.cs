33fb89a352759884f0857efcc08cb4330adf0cbb
Hotskillet <124469811+Hotskillet@users.noreply.github.com>
2023-04-29 14:41:24 -0700

    added scripts and learned thigns about collisions and vectors

---
+++ unityIntro/Assets/scriptthing.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptthing : MonoBehaviour
+{   
+    //defining some stuff according to the class
+    
+    //defining an integer called myInt 
+    //specifics about public: it affects scope as how i expected it to work.
+    //public means we can reference myInt outside of this class
+    public int myInt;
+
+    // Start is called before the first frame update
+    //so everything in void start is called only once
+    //functions are named by what they return
+    //this function doesnt return anything, so its defined as void
+    //if i wanted this to return a number, then it be int Start(par)
+    void Start() {
+        myInt = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        myInt += 1;
+        Debug.Log(myInt);
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
